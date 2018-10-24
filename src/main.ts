6b63c503844806293cbaee0b51234882bab32dff
Weiting <a26007565@gmail.com>
2018-10-24 15:15:40 +0800

    Change using auth to get token.

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,27 @@ import { MailService } from './services/mailService';
 dotenv.config({ allowEmptyValues: true });
 dotenv.load();
 
-const cookie: string = process.env.DCARD_COOKIE || '';
-const xCsrfToken: string = process.env.DCARD_XCSRF_TOKEN || '';
+const username: string = process.env.DCARD_USERNAME || '';
+const password: string = process.env.DCARD_PASSWORD || '';
 
 const acceptGender: number = Number.parseInt(process.env.DCARD_ACCEPT_GENDER, 10);
-const dcardService: DcardService = new DcardService(cookie, xCsrfToken);
+const dcardService: DcardService = new DcardService();
 const mailService: MailService = new MailService();
 
-dcardService.getDcardInfo().then(
-  async (dcardInfo) => {
-    console.log(JSON.stringify(dcardInfo));
-    if (acceptGender > 1 || Gender[acceptGender] === dcardInfo.dcard.gender) {
-      await mailService.sendDcardInfo(dcardInfo);
-      const result: { bothAccept: boolean } = await dcardService.sendAccept();
-      console.log('sent an invitation card');
-      console.log(`bothAccept: ${result.bothAccept}`);
-      return;
-    }
+dcardService.login(username, password).then(
+  (success) => {
+    if (!success) { return; }
+    dcardService.getDcardInfo().then(
+      async (dcardInfo) => {
+        console.log(JSON.stringify(dcardInfo));
+        if (acceptGender > 1 || Gender[acceptGender] === dcardInfo.dcard.gender) {
+          await mailService.sendDcardInfo(dcardInfo);
+          const result: { bothAccept: boolean } = await dcardService.sendAccept();
+          console.log('sent an invitation card');
+          console.log(`bothAccept: ${result.bothAccept}`);
+          return;
+        }
+      }
+    );
   }
 );
-
--- src/services/dcardService.ts
+++ src/services/dcardService.ts
@@ -1,29 +1,21 @@
 // import * as dotenv from 'dotenv-safe';
 import * as request from 'request';
-import { DcardInfo } from './../models/cardInfo';
+import { DcardInfo } from './../models/';
 
 const hostName: string = 'https://www.dcard.tw';
 const dacrdApi: string = `${hostName}/_api/dcard`;
 const acceptApi: string = `${hostName}/_api/dcard/accept`;
+const sessionUrl: string = `${hostName}/_api/sessions`;
 
 export class DcardService {
   public static $inject: string[] = ['request'];
 
-  constructor(
-    private cookie: string,
-    private xCsrfToken: string
-  ) {
-  }
-
-  /**
-   * getDcardInfo
-   */
   public getDcardInfo(): Promise<DcardInfo> {
     return new Promise<DcardInfo>((resolve, reject) => {
       request.get({
         url: dacrdApi,
         headers: {
-          Cookie: this.cookie
+          Cookie: process.env.cookie
         },
         followAllRedirects: true
       }, (error, response, body) => {
@@ -33,7 +25,6 @@ export class DcardService {
     });
   }
 
-
   public sendAccept(message: string = 'hi'): Promise<{ bothAccept: boolean }> {
     return new Promise<{ bothAccept: boolean }>((resolve, reject) => {
       request.post({
@@ -41,19 +32,40 @@ export class DcardService {
         headers: {
           'Content-Type': 'application/json',
           'Cache-Control': 'no-cache',
-          'Cookie': this.cookie,
-          'x-csrf-token': this.xCsrfToken,
+          'Cookie': process.env.cookie,
+          'x-csrf-token': process.env.xCsrfToken,
         },
         json: {
           firstMessage: message
         }
-        // headers: response.headers
       }, (error, response, body) => {
-        // if (!error) {
-        //   console.log(response.request.headers);
-        //   console.log(body);
-        // }
         if (!error) { resolve(JSON.parse(JSON.stringify(body))); }
+        reject();
+      });
+    });
+  }
+
+  public login(username: string, password: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      request.post({
+        url: sessionUrl,
+        headers: {
+          // tslint:disable-next-line:max-line-length
+          'Cookie': '__cfduid=dee5a35874981592fa33ffa366c47925e1540358599; dcard=eyJjc3JmU2VjcmV0IjoiQ0p6UjA2dFRtM3hXLUtQaE1Tc1F0NzVMIn0=; dcard.sig=G95BMTHNwq0XYrhKuuKOHb1ssCI',
+          'x-csrf-token': 'yGLNKTin-ZzdVnBhgW1mZ1Bhl_8BcF-G-rcg'
+        },
+        json: {
+          email: username,
+          password
+        }
+      }, (error, response, body) => {
+        if (response.statusCode === 204) {
+          process.env.xCsrfToken = response.headers['x-csrf-token'].toString();
+          process.env.cookie = response.headers['set-cookie'].map(x => x.split(';')[0]).join('; ');
+          resolve(true);
+        } else {
+          resolve(false);
+        }
         reject();
       });
     });
