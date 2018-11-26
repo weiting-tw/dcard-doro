495e50edc646cc0e2b8b04fa0b24f049b540c9b4
Weiting <a26007565@gmail.com>
2018-11-26 11:17:55 +0800

    Fix line notify template

--- src/utils/dcardHelper.ts
+++ src/utils/dcardHelper.ts
@@ -2,7 +2,6 @@ import { Dcard } from './../models/cardInfo';
 export class DcardHelper {
   public toLineMsg(dcard: Dcard): string {
     return `\r
-      You get the new card friend \r
       學校: ${dcard.school} \r
       科系: ${dcard.department} \r
       興趣: ${dcard.talent} \r
