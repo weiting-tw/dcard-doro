a89388954b25821f03a351b7cbdf00014143cfd5
weiting <a26007565@gmail.com>
2019-01-26 01:40:35 +0800

    加入專案檔案。

---
+++ Dcard-Doro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dcard_Doro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
